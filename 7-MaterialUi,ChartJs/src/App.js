f1e076ad25230f14d0d22d8e72731d1c6badc0ac
Abdur Rehman <abdulrahman.siddiqui16@gmail.com>
2020-09-12 08:53:29 +0500

    MaterialUI

---
+++ 7-MaterialUi,ChartJs/src/App.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import './App.css';
+import NavBar from './NavBar.js';
+import { makeStyles } from '@material-ui/core/styles';
+import {Paper, TextField, Button, CircularProgress} from '@material-ui/core';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  root2:{
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    } 
+  },
+  paper: {
+    height: 350,
+    width: 300,
+    margin: '0 auto',  
+    textAlign: "center",
+  }
+}));
+
+// 50 minutes onwards
+
+function App() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+
+      <NavBar />
+      
+    <Paper elevation={3} className={classes.paper}> 
+      <h1 style= {{paddingTop: "20px"}} >Login</h1>
+      <form className={classes.root2} noValidate autoComplete="off">
+      <TextField id="standard-basic" label="Enter E-mail" />
+      <TextField id="standard-basic" label="Enter Password" />
+      <Button variant="contained" color="primary" style={{marginTop: "40px"}}>
+        Submit
+      </Button>
+      </form>
+    </Paper>
+   
+  </div>
+  );
+}
+
+export default App;
---
+++ 7-MaterialUi,ChartJs/src/NavBar.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+// 22 minutes onwards
+
+function NavBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Expense Builder
+          </Typography>
+        </Toolbar>
+      </AppBar>
+  </div>
+  );
+
+}
+
+export default NavBar;
