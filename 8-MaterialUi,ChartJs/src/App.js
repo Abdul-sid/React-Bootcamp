02bb49382db1134c6416c864008c90321df3b3c4
Abdur Rehman <abdulrahman.siddiqui16@gmail.com>
2020-09-13 18:14:26 +0500

    covid19 tracker

---
+++ 8-MaterialUi,ChartJs/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import AppBar from './components/appBar.js';
+import Grid from './components/grid.js';
+
+function App() {
+
+// 26 minutes onward
+
+  return (
+    <div>
+      <AppBar />
+      <Grid />
+    </div>
+  );
+}
+
+export default App;
